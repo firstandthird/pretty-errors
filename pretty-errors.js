eb2b488f09ff2386f80a81199b21800935a8f946
Manosfrias <a.gilamor@gmail.com>
2018-12-04 10:06:56 +0100

    make errors rules extensible

---
+++ pretty-errors.js
@@ -0,0 +1,37 @@
+import { on, html, addClass, fire } from 'domassist';
+import Domodule from 'domodule';
+import ErrorRules from './error-rules';
+
+class PrettyErrors extends Domodule {
+  getRules() {
+    return null;
+  }
+
+  postInit() {
+    on(document.body, this.options.event, this.onError.bind(this));
+    fire(document.body, this.options.event, { detail: { error: 'error' } });
+  }
+
+  onError(event) {
+    let message = event.detail.error;
+    const rules = this.getRules();
+
+    if (rules) {
+      rules.some(er => {
+        const match = er.regexp.exec(message);
+        er.regexp.lastIndex = 0;
+
+        if (match) {
+          message = er.format(match);
+        }
+
+        return !!match;
+      });
+    }
+
+    addClass(this.el, 'visible');
+    html(this.el, message);
+  }
+}
+
+Domodule.register('PrettyErrors', PrettyErrors);
