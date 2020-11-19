08fd57d35efd39dfffb0243df0ac6e32b4a0e4b5
Troy Whiteley <24781+dawnerd@users.noreply.github.com>
2020-11-19 00:10:02 +0000

    chore(Deps): update deps

--- test/pretty-errors.test.js
+++ test/pretty-errors.test.js
@@ -1,7 +1,6 @@
-import PrettyErrors from '../index';
+import PrettyErrors from '..';
 import Domodule from 'domodule';
 import { fire } from 'domassist';
-import test from 'tape-rollup';
 
 const RULES = [
   {
@@ -25,59 +24,52 @@ const setup = options => {
   return PrettyErrors.discover();
 };
 
-test('Class', assert => {
-  assert.ok(PrettyErrors.prototype instanceof Domodule, 'It\'s a domodule class');
-  assert.end();
+test('Class', () => {
+  expect(PrettyErrors.prototype instanceof Domodule, 'It\'s a domodule class').toBeTruthy();
 });
 
-test('Same event, not matching error message', assert => {
+test('Same event, not matching error message', () => {
   const [instance] = setup('circle:create:error');
 
   fire(document.body, 'circle:create:error', { detail: { error: 'error' } });
-  assert.equal(instance.el.innerText, 'error');
-  assert.end();
+  expect(instance.el.innerText).toMatch('false');
 });
 
-test('Same event, matching error message', assert => {
+test('Same event, matching error message', () => {
   const [instance] = setup('circle:create:error');
 
   instance.getRules = () => RULES;
 
   fire(document.body, 'circle:create:error', { detail: { error: 'apiQuery:error' } });
-  assert.equal(instance.el.innerText, 'Api fails');
-  assert.end();
+  expect(instance.el.innerText).toMatch('Api fails');
 });
 
-test('Multiple events, same error type', assert => {
+test('Multiple events, same error type', () => {
   const [instance] = setup('circle:create:error, circle:error, api:Error');
 
   instance.getRules = () => RULES;
 
   fire(document.body, 'circle:create:error', { detail: { error: 'circle not create' } });
-  assert.equal(instance.el.innerText, 'circle not create');
+  expect(instance.el.innerText).toMatch('circle not create');
 
   fire(document.body, 'circle:error', { detail: { error: 'error' } });
-  assert.equal(instance.el.innerText, 'error');
+  expect(instance.el.innerText).toMatch('error');
 
   fire(document.body, 'api:Error', { detail: { error: 'apiQuery:error' } });
-  assert.equal(instance.el.innerText, 'Api fails');
-
-  assert.end();
+  expect(instance.el.innerText).toMatch('Api fails');
 });
 
-test('If events dont match not change inner text', assert => {
+test('If events dont match not change inner text', () => {
   const [instance] = setup('circle:create:error');
 
   fire(document.body, 'random-event', { detail: { error: 'error' } });
-  assert.notEqual(instance.el.innerText, 'error');
-  assert.end();
+  expect(instance.el.innerText).not.toMatch('error');
 });
 
-test('Required event', assert => {
+test('Required event', () => {
   const wrongEl = document.createElement('div');
 
-  assert.throws(() => {
+  expect(() => {
     new PrettyErrors(wrongEl);
-  }, 'Should throw if required option is missing');
-  assert.end();
+  }).toThrow();
 });
---
+++ test/setup.js
@@ -0,0 +1,1 @@
+global.PrettyErrors = require('..');
